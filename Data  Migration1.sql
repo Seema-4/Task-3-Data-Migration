2a727a4351c0a70d831b9264716e1c9d3e3b2bd7
Seema-4 <tsseemaafroz@gmail.com>
2025-07-24 15:16:24 +0530

    Add files via upload

---
+++ Data  Migration1.sql
@@ -0,0 +1,9 @@
+SELECT * FROM data.staffs;
+
+INSERT INTO staffs (StaffID, StaffName, DeptID)
+VALUES 
+  (14, 'Seema Afroz', 104),
+  (15, 'Nandini Rao', 105),
+  (16, 'Aditya Sharma', 106),
+  (17, 'Kiran Reddy', 107),
+  (18, 'Farhan Khan', 108);
